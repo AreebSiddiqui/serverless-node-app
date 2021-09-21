9981dafa6aa1deffebb4cdbac78fc82cabf33cd8
AreebSiddiqui <areeb40@hotmail.com>
2021-09-22 01:01:35 +0500

    added conf

--- handler.js
+++ handler.js
@@ -2,14 +2,41 @@
 require('dotenv').config();
 const { Pool, Client } = require('pg')
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!'
-      },
-      null,
-      2
-    ),
+  
+  const QUERY = 'SELECT name from public."city"';
+
+
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+    client.connect();
+    try {
+      let result = await client.query(QUERY);
+      if (result) {
+       await client.end();
+        return {
+          statusCode: 200,
+          body: JSON.stringify(
+            {
+              message: 'Query executed successfully!'
+            }),
+        };
+         
+      } 
+    }
+    catch(e){ 
+      return {
+        statusCode: 400,
+        body: JSON.stringify(
+          {
+            message: 'Not executed successfully'
+          }
+        ),
+      };
   };
-};
+}
