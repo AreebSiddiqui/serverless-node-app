1822073b8ef67c4ab02de6f54e3d11a96ca3474c
AreebSiddiqui <areeb40@hotmail.com>
2021-09-22 11:26:49 +0500

    added conf

--- handler.js
+++ handler.js
@@ -2,21 +2,25 @@
 require('dotenv').config();
 const { Pool, Client } = require('pg')
 module.exports.hello = async event => {
-  
+  console.log('FIRST CONSOLE')
   const QUERY = 'SELECT name from public."city"';
-
-
+  
+  console.log('FIRST CONSOLE');
+  
   const client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-});
-
-      await client.connect();
-      let result = await client.query(QUERY);
-      if (result) {
+  });
+  console.log('SECOND CONSOLE');
+  
+  await client.connect();
+  console.log('THIRD CONSOLE');
+  let result = await client.query(QUERY);
+  if (result) {
+        console.log('FOUR CONSOLE');
        await client.end();
        return {
         statusCode: 200,
@@ -29,7 +33,7 @@ module.exports.hello = async event => {
         ),
       };
     }
-    
+  console.log('FIVE CONSOLE');
     return {
       statusCode: 200,
       body: JSON.stringify(
